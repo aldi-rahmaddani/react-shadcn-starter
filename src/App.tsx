fab48fbfcb786968a8d19cc339088011e6b59bfa
Aldi Rahmaddani <aldi.rahmaddani12@gmail.com>
2025-03-08 11:04:49 +0700

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="h-dvh flex justify-center items-center bg-gray-100">
+        <p className="text-lg text-blue-500 font-medium font-sans">
+          Hello👋, good luck working on your project 😁
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
